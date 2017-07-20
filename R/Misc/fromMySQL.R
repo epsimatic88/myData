c2c30e6ddef14d5f40af3ebbd6dabe59d397f0ee
williamlfang <william.lian.fang@gmail.com>
2017-07-20 13:15:38 +0800

    china_futures_HFT 高频数据只提取 >= 2016 年之后的

--- R/Misc/fromMySQL.R
+++ R/Misc/fromMySQL.R
@@ -1,6 +1,6 @@
 ################################################################################
 ##! fromMySQL.R
-## 
+##
 ## 从　MySQL 数据库提取数据
 ## 并保存为 fst 格式
 ##
@@ -29,7 +29,8 @@ library(fst)
 
 ## =============================================================================
 dataPath <- "./data/FromMySQL"
-allDataBases <- c('china_futures_HFT', 'china_futures_bar', 'china_futures_info',
+allDataBases <- c(#'china_futures_HFT',
+                  'china_futures_bar', 'china_futures_info',
                   'dev', 'jydb', 'HiCloud', 'vnpy', 'YY_SimNow')
 for (i in allDataBases) {
     if (! dir.exists(paste(dataPath, i, sep = '/'))) {
@@ -45,6 +46,7 @@ for (i in allDataBases) {
 ## 读取命令: read.fst(file, as.data.table = TRUE)
 ## =============================================================================
 for (i in allDataBases) {
+    print(i)
     ## -------------------------------------------------------------------------
     mysql <- mysqlFetch(i)
     allTables <- dbListTables(mysql)
@@ -52,6 +54,7 @@ for (i in allDataBases) {
 
     ## -------------------------------------------------------------------------
     for (k in 1:length(allTables)) {
+        print(k)
         dt <- dbGetQuery(mysql, paste("
                         SELECT * FROM", allTables[k]))
         ## ---------------------------------------------------------------------
@@ -63,4 +66,35 @@ for (i in allDataBases) {
         ## ---------------------------------------------------------------------
     }
     ## -------------------------------------------------------------------------
+    for( conns in dbListConnections(MySQL()) ){
+      dbDisconnect(conns)
+    }
+}
+
+## =============================================================================
+## china_futures_HFT
+## =============================================================================
+dataPath <- "./data/FromMySQL"
+if (! dir.exists(paste(dataPath, 'china_futures_HFT', sep = '/'))) {
+    dir.create(paste(dataPath, 'china_futures_HFT', sep = '/'))
+}
+mysql <- mysqlFetch('china_futures_HFT')
+allTables <- dbListTables(mysql)
+
+for (k in allTables) {
+    mysql <- mysqlFetch('china_futures_HFT')
+    print(k)
+    dt <- dbGetQuery(mysql, paste("
+            SELECT * FROM", k,
+            "WHERE YEAR(TradingDay) >=", 2016))
+    ## -------------------------------------------------------------------------
+    if (nrow(dt) == 0) {
+        dt[1, colnames(dt)] <- NA
+    } else {
+        write.fst(dt, path = paste0(dataPath, '/', 'china_futures_HFT', '/', k, '.fst'))
+    }
+    ## -------------------------------------------------------------------------
+    for( conns in dbListConnections(MySQL()) ){
+      dbDisconnect(conns)
+    }
 }
