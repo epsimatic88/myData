d671579d6ba227c43d45a1ba161ee0de18c9c164
williamlfang <william.lian.fang@gmail.com>
2017-10-18 10:16:00 +0800

    中金所 oiRank 数据爬虫

--- R/oiRank/R/cffex.R
+++ R/oiRank/R/cffex.R
@@ -5,24 +5,34 @@
 ## Author: William Fang
 ## Date  : 2017-08-21
 ################################################################################
+
+################################################################################
+## STEP 0: 初始化，载入包，设定初始条件
+################################################################################
 rm(list = ls())
+logMainScript <- c("cffex.R")
 
-library(data.table)
-library(magrittr)
-library(RSelenium)
-library(parallel)
+setwd('/home/fl/myData/')
+suppressMessages({
+  source('./R/Rconfig/myInit.R')
+})
 
 ################################################################################
-setwd("/home/william/Documents/oiRank")
+## STEP 1: 获取对应的交易日期
 ################################################################################
-
-ChinaFuturesCalendar <- fread("./R/ChinaFuturesCalendar.csv") %>% 
-  .[days <= gsub("-","",Sys.Date() - 1)] %>% 
-  .[,.(days)]
+ChinaFuturesCalendar <- fread("./data/ChinaFuturesCalendar/ChinaFuturesCalendar.csv",
+                              colClasses = list(character = c("nights","days"))) %>% 
+                              .[days < format(Sys.Date(),'%Y%m%d')]
 
 exchCalendar <- ChinaFuturesCalendar[,":="(calendarYear = substr(days,1,4),
                                            calendarYearMonth = substr(days,1,6),
+                                           calendarMonth = substr(days,5,6),
                                            calendarDay = substr(days,7,8))]
+exchURL <- "http://www.cffex.com.cn/fzjy/ccpm/"
+
+dataPath <- '/home/william/Documents/oiRank/CFFEX/'
+# dataPath <- "./data/oiRank/CFFEX/"
+
 ################################################################################
 ## CFFEX: 中期所 
 ##        
@@ -32,77 +42,46 @@ exchCalendar <- ChinaFuturesCalendar[,":="(calendarYear = substr(days,1,4),
 ## 4. T ：10年期国债    --> 2015-03-20
 ## 5. TF：5年期国债     --> 2013-09-06
 ################################################################################
-exchURL <- "http://www.cffex.com.cn/fzjy/ccpm/"
 
-dataSet <- data.table(productID = c('IC','IF','IH','T','TF'),
-                      startDate = c('20150416','20100416','20150416',
-                                    '20150320','20130906'))
+## -----------------------------------------------------------------------------
+productSet <- data.table(productID = c('IC','IF','IH','T','TF'),
+                         startDate = c('20150416','20100416','20150416',
+                                       '20150320','20130906'))
 
-#-------------------------------------------------------------------------------
-# 1.持仓排名
-for (i in 1:nrow(dataSet)) {
-  
-  tempCalendar <- exchCalendar[days >= dataSet[i,startDate]]
-  
-  ## ===========================================================================
-  ## 设置路径
-  ## ---------------------------------------------------------------------------
-  tempPath <- "./data/CFFEX"
-  if (!dir.exists(tempPath)) dir.create(tempPath)
-  setwd(tempPath)
-  ## ===========================================================================
-  
-  ## ===========================================================================
-  ## 开始下载数据
-  ## ---------------------------------------------------------------------------
-  for (tempYear in tempCalendar[,unique(calendarYear)]) {
+productCalenar <- lapply(productSet[,productID], function(id) {
+  res <- exchCalendar[days >= productSet[productID == id, startDate]] %>% 
+        .[, productID := id]
+}) %>% rbindlist()
+## -----------------------------------------------------------------------------
+
+## -----------------------------------------------------------------------------
+cffexData <- function(calendarYear, calendarMonth, calendarDay, productID) {
+    tempURL <- paste0(exchURL, paste0(calendarYear, calendarMonth), '/', calendarDay, '/',
+                      productID,'_1.csv')
+
+    destFile <-  paste0(dataPath, calendarYear, '/', 
+                      paste0(calendarYear, calendarMonth, calendarDay),
+                      '_',productID,'.csv')
+
     ## -------------------------------------------------------------------------
-    ## 1. 
+    while(! file.exists(destFile) | file.size(destFile) < 1000){
+      try(download.file(tempURL, destFile, mode = 'wb'))
+    }
     ## -------------------------------------------------------------------------
-    if(!dir.exists(tempYear)) dir.create(tempYear)
-    ## -------------------------------------------------------------------------
-    tempYearCalendar <- tempCalendar[calendarYear == tempYear]
-    ## =========================================================================
-    # for (k in 1:nrow(tempYearCalendar)) {
-    #   tempURL <- paste0(exchURL, tempYearCalendar[k,calendarYearMonth],'/',
-    #                     tempYearCalendar[k,calendarDay],'/',
-    #                     dataSet[i,productID],'_1.csv')
-
-    #   destFile <-  paste0("./",tempYear,'/',tempCalendar[calendarYear == tempYear][k,days]
-    #                       ,'_',dataSet[i,productID],'.csv')
-
-    #   ## -----------------------------------------------------------------------
-    #   # while(! file.exists(destFile) | file.size(destFile) < 3000){
-    #   while(! file.exists(destFile) | file.size(destFile) < 2000){
-    #     try(download.file(tempURL, destFile, mode = 'wb'))
-    #     # Sys.sleep(10)
-    #   }
-    #   ## -----------------------------------------------------------------------
-    # }
-
-    cl <- makeCluster(round(detectCores()/4*3), type = 'FORK')
-    parSapply(cl, 1:nrow(tempYearCalendar), function(k){
-      tempURL <- paste0(exchURL, tempYearCalendar[k,calendarYearMonth],'/',
-                        tempYearCalendar[k,calendarDay],'/',
-                        dataSet[i,productID],'_1.csv')
-
-      destFile <-  paste0("./",tempYear,'/',tempCalendar[calendarYear == tempYear][k,days]
-                          ,'_',dataSet[i,productID],'.csv')
-
-      ## -----------------------------------------------------------------------
-      # while(! file.exists(destFile) | file.size(destFile) < 3000){
-      while(! file.exists(destFile) | file.size(destFile) < 1000){
-        try(download.file(tempURL, destFile, mode = 'wb'))
-        # Sys.sleep(10)
-      }
-      ## -----------------------------------------------------------------------
-    })
-    stopCluster(cl)
-  ## ===========================================================================
-  }
-
-  ## ===========================================================================
-  ##--- 返回上一层目录
-  setwd("../..")
-  ## ===========================================================================
 }
+## -----------------------------------------------------------------------------
+
+
+################################################################################
+## STEP 2: 开启并行计算模式，下载数据 
+################################################################################
+cl <- makeCluster(max(round(detectCores()*3/4),4), type='FORK')
+parSapply(cl, 1:nrow(productCalenar), function(i){
+  ## ---------------------------------------------------------------------------
+  tempDir <- paste0(dataPath, productCalenar[i, calendarYear])
+  if (!dir.exists(tempDir)) dir.create(tempDir, recursive = TRUE)
+
+  productCalenar[i, cffexData(calendarYear, calendarMonth, calendarDay, productID)]
+  ## ---------------------------------------------------------------------------
+})
+stopCluster(cl)
