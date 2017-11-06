582976dd5449506fb4299fbab233e394b502b6b5
williamlfang <william.lian.fang@gmail.com>
2017-11-06 15:26:27 +0800

    完成 czce 持仓排名数据爬虫

--- R/oiRank/R/czce.R
+++ R/oiRank/R/czce.R
@@ -5,24 +5,41 @@
 ## Author: William Fang
 ## Date  : 2017-08-21
 ################################################################################
-rm(list = ls())
 
-library(data.table)
-library(magrittr)
+################################################################################
+## STEP 0: 初始化，载入包，设定初始条件
+################################################################################
+rm(list = ls())
+logMainScript <- c("czce.R")
+
+# setwd('/home/fl/myData/')
+if (class(try(setwd('/home/fl/myData/'))) == 'try-error') {
+  setwd('/run/user/1000/gvfs/sftp:host=192.168.1.166,user=fl/home/fl/myData')
+}
+
+suppressMessages({
+  source('./R/Rconfig/myInit.R')
+})
 library(RSelenium)
-library(parallel)
-library(rvest)
 Sys.setlocale("LC_ALL", 'en_US.UTF-8')
 
-################################################################################
-setwd("/home/william/Documents/oiRank")
-################################################################################
+ChinaFuturesCalendar <- fread("./data/ChinaFuturesCalendar/ChinaFuturesCalendar.csv",
+                              colClasses = list(character = c("nights","days"))) %>% 
+                              .[days < format(Sys.Date(),'%Y%m%d')]
 
-ChinaFuturesCalendar <- fread("./R/ChinaFuturesCalendar.csv") %>% 
-  .[days <= gsub("-","",Sys.Date() - 1)] %>% 
-  .[,.(days)]
+exchCalendar <- ChinaFuturesCalendar[,":="(calendarYear = substr(days,1,4),
+                                           calendarYearMonth = substr(days,1,6),
+                                           calendarMonth = substr(days,5,6),
+                                           calendarDay = substr(days,7,8))]
+dataPath <- '/home/william/Documents/oiRank/CZCE/'
+# dataPath <- "./data/Bar/Exchange/CZCE/"
 
-calendarYear <- ChinaFuturesCalendar[, unique(substr(days,1,4))]
+##------------------------------------------------------------------------------
+if(Sys.info()['sysname'] == 'Windows'){
+  Sys.setenv("R_ZIPCMD" = "D:/Program Files/Rtools/bin/zip.exe") ## path to zip.exe
+}
+##------------------------------------------------------------------------------
+
 ################################################################################
 ## CZCE: 郑商所
 ## 1.持仓排名
@@ -36,95 +53,81 @@ exchURL2 <- "http://www.czce.com.cn/portal/DFSStaticFiles/Future/"
 ## =============================================================================
 
 
-## =============================================================================
-## 
-## -----------------------------------------------------------------------------
-for(tempYear in calendarYear){
-  
-  tempDays <- ChinaFuturesCalendar[substr(days,1,4) == tempYear]
-  
-  ## ===========================================================================
-  ## 设置路径
-  ## ---------------------------------------------------------------------------
-  tempPath <- "./data/CZCE/"
-  if (!dir.exists(tempPath)) dir.create(tempPath)
+czceData <- function(i) {
+  tempDir <- paste0(dataPath,exchCalendar[i,calendarYear])
 
-  setwd(tempPath)
-  if(!dir.exists(tempYear)) dir.create(tempYear)
-  ## ===========================================================================
-  
-  ## ===========================================================================
-  # for(k in 1:nrow(tempDays)){
-  #   x <- tempDays[k,days]
-  #   tempURL <- ifelse(x < '20151001',
-  #                     paste0(exchURL1, tempYear, '/datatradeholding/', x, '.txt'),
-  #                     paste0(exchURL2, tempYear, '/', x, '/FutureDataHolding.xls'))
-    
-  #   destFile <-  paste0("./",tempYear,"/",x,
-  #                       ifelse(x < '20151001','.txt','.xls'))
-  #   ## -------------------------------------------------------------------------
-  #   while(!file.exists(destFile) | file.size(destFile) < 1000){
-  #     try(download.file(tempURL, destFile, mode = 'wb'))
-  #   }
-  #   ## -------------------------------------------------------------------------
-  # }
+  if (!dir.exists(tempDir)) dir.create(tempDir, recursive = TRUE)
 
-    cl <- makeCluster(round(detectCores()/4*3), type = 'FORK')
-    parSapply(cl, 1:nrow(tempDays), function(k){
-      x <- tempDays[k,days]
-      tempURL <- ifelse(x < '20151001',
-                        paste0(exchURL1, tempYear, '/datatradeholding/', x, '.txt'),
-                        paste0(exchURL2, tempYear, '/', x, '/FutureDataHolding.xls'))
+  tempYear <- exchCalendar[i,calendarYear]
+  tempTradingDay <- exchCalendar[i,days]
       
-      destFile <-  paste0("./",tempYear,"/",x,
-                          ifelse(x < '20151001','.txt','.xls'))
-      ## -------------------------------------------------------------------------
-      while(!file.exists(destFile) | file.size(destFile) < 1000){
-        if (class(try(download.file(tempURL, destFile, mode = 'wb'))) == 'try-error') {
-          tempPage <- paste0('http://www.czce.com.cn/portal/exchange/jyxx/pm/pm', x, '.html')
-          
-          tempData <- tempPage %>% 
-                      read_html(encoding = 'GB18030') %>% 
-                      html_nodes('table') %>% 
-                      html_table(fill=TRUE, header=FALSE) %>% 
-                      .[-1] %>% 
-                      .[[1]] %>% 
-                      as.data.table() %>% 
-                      rbind(data.table(X1 = c('','')), ., fill = TRUE)
-          tempData[1, X1 := paste0('郑州商品交易所持仓排行表(',
-                                   as.Date(as.character(tempDays[k,days]), format = '%Y%m%d'),
-                                   ')')]
-          cols <- colnames(tempData)[2:ncol(tempData)]
-          tempData[, (cols) := lapply(.SD, function(x){
-            gsub(',','',x)
-          }), .SDcols = cols]
-
-          # grep("名次", tempData$X1) %>% length()
-
-          tempTitle <- tempPage %>% 
-                      read_html(encoding = 'GB18030') %>% 
-                      html_nodes('font') %>% 
-                      html_text() %>% 
-                      .[grep('品种|合约代码',.)]
-          # length(tempTitle)
-
-          for (j in 1:length(tempTitle)) {
-            tempRow <- grep("名次", tempData$X1)[j] - 1
-            tempData[tempRow, X1 := tempTitle[j]]
-          }
-
-          fwrite(tempData, destFile, col.names = FALSE)
-        }
-      }
-      ## -------------------------------------------------------------------------
-    })
-    stopCluster(cl)
-  ## ===========================================================================
-
-  ## ===========================================================================
+  tempURL <- ifelse(tempTradingDay < '20151001',
+                    paste0(exchURL1, tempYear, '/datatradeholding/', tempTradingDay, '.txt'),
+                    paste0(exchURL2, tempYear, '/', tempTradingDay, '/FutureDataHolding.xls'))
   
-  ## ===========================================================================
-  ##--- 返回上一层目录
-  setwd("../..")
-  ## ===========================================================================
+  destFile <-  paste0(dataPath, '/', exchCalendar[i,calendarYear],
+                      "/", tempTradingDay,
+                      ifelse(tempTradingDay < '20151001','.txt','.xls'))
+
+  tryNo <- 0
+  ## ---------------------------------------------------------------------------
+  while( (!file.exists(destFile) | file.size(destFile) < 1000) & (tryNo < 20)){
+    if (class(try(download.file(tempURL, destFile, mode = 'wb'))) == 'try-error') {
+      tempPage <- paste0('http://www.czce.com.cn/portal/exchange/jyxx/pm/pm', tempTradingDay, '.html')
+      
+      webData <- tempPage %>% 
+                  read_html(encoding = 'GB18030') %>% 
+                  html_nodes('table') %>% 
+                  html_table(fill=TRUE, header=FALSE) %>% 
+                  .[-1] %>% 
+                  .[[1]] %>% 
+                  as.data.table() %>% 
+                  rbind(data.table(X1 = c('','')), ., fill = TRUE)
+      webData[1, X1 := paste0('郑州商品交易所持仓排行表(',
+                               as.Date(as.character(tempTradingDay), format = '%Y%m%d'),
+                               ')')]
+
+      cols <- colnames(webData)[2:ncol(webData)]
+      webData[, (cols) := lapply(.SD, function(x){
+        gsub(',','',x)
+      }), .SDcols = cols]
+
+      # grep("名次", tempData$X1) %>% length()
+
+      webTitle <- tempPage %>% 
+                  read_html(encoding = 'GB18030') %>% 
+                  html_nodes('font') %>% 
+                  html_text() %>% 
+                  .[grep('品种|合约代码',.)]
+
+      for (j in 1:length(webTitle)) {
+        tempRow <- grep("名次", webData$X1)[j] - 1
+        webData[tempRow, X1 := webTitle[j]]
+      }
+
+      print(webData)
+
+      fwrite(webData, destFile, col.names = FALSE)
+    }
+    tryNo <- tryNo + 1
+  }
+  ## ---------------------------------------------------------------------------
 }
+
+################################################################################
+## STEP 2: 开启并行计算模式，下载数据 
+################################################################################
+cl <- makeCluster(max(round(detectCores()*3/4),4), type='FORK')
+parSapply(cl, 1:nrow(ChinaFuturesCalendar), function(i){
+  ## ---------------------------------------------------------------------------
+  try(czceData(i))
+  ## ---------------------------------------------------------------------------
+})
+stopCluster(cl)
+
+
+# ## =============================================================================
+# sapply(1:nrow(ChinaFuturesCalendar), function(i){
+#   try(czceData(i))
+# })
+# ## =============================================================================
