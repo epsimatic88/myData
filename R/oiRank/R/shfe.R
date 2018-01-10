6bd0d6c843e45a6754e35707e5202c3e2fbb7a76
williamlfang <william.lian.fang@gmail.com>
2018-01-10 21:04:45 +0800

    重新编写上期所持仓排名数据，实现 post 获取数据

--- R/oiRank/R/shfe.R
+++ R/oiRank/R/shfe.R
@@ -1,136 +1,76 @@
 ################################################################################
-## shfe.R
-## 用于下载上期所期货公司持仓排名数据
-##
-## Author: William Fang
-## Date  : 2017-08-21
+ROOT_PATH <- '/home/fl/myData'
+DATA_PATH <- './data/oiRank/data'
+
+setwd(ROOT_PATH)
+source('./R/Rconfig/myInit.R')
+library(httr)
+library(rjson)
 ################################################################################
 
+ChinaFuturesCalendar[, nights := gsub('-', '', nights)]
+ChinaFuturesCalendar[, days := gsub('-', '', days)]
+ChinaFuturesCalendar <- ChinaFuturesCalendar[days <= format(Sys.Date(), '%Y%m%d')]
 
-################################################################################
-## STEP 0: 初始化，载入包，设定初始条件
-################################################################################
-rm(list = ls())
-logMainScript <- c("shfe.R")
-
-if (class(try(setwd('/home/fl/myData/'))) == 'try-error') {
-  setwd('/run/user/1000/gvfs/sftp:host=192.168.1.166,user=fl/home/fl/myData')
-}
-suppressMessages({
-  source('./R/Rconfig/myInit.R')
+allYears <- ChinaFuturesCalendar[, unique(substr(days, 1, 4))]
+sapply(allYears, function(i){
+    tempDir <- paste0(DATA_PATH, '/SHFE/', i)
+    if (!dir.exists(tempDir)) dir.create(tempDir, recursive = T)
 })
-library(RSelenium)
-################################################################################
-## STEP 1: 获取对应的交易日期
-################################################################################
-ChinaFuturesCalendar <- fread("./data/ChinaFuturesCalendar/ChinaFuturesCalendar.csv",
-                              colClasses = list(character = c("nights","days"))) %>% 
-                              .[days < format(Sys.Date(),'%Y%m%d')]
 
-exchCalendar <- ChinaFuturesCalendar[,":="(calendarYear = substr(days,1,4),
-                                           calendarYearMonth = substr(days,1,6),
-                                           calendarMonth = substr(days,5,6),
-                                           calendarDay = substr(days,7,8))]
-dataPath <- '/home/william/Documents/oiRank/SHFE/'
-# dataPath <- "./data/Bar/oiRank/SHFE/"
 
-##------------------------------------------------------------------------------
-if(Sys.info()['sysname'] == 'Windows'){
-  Sys.setenv("R_ZIPCMD" = "D:/Program Files/Rtools/bin/zip.exe") ## path to zip.exe
+
+## -----------------------------------------------------------------------------
+for (d in 1:nrow(ChinaFuturesCalendar)) { # nrow(ChinaFuturesCalendar)
+    ## -------------------------------------------------------------------------
+    # tradingDay <- '20180110'
+    tradingDay <- ChinaFuturesCalendar[d, days]
+    shfeYear <- substr(tradingDay, 1, 4)
+    destFile <- paste0(DATA_PATH, '/SHFE/', shfeYear, '/', tradingDay, '.csv')
+
+    if (file.exists(destFile)) next
+    ## -------------------------------------------------------------------------
+
+    tryNo <- 0
+
+    ## =========================================================================
+    while (tryNo < 10 & !file.exists(destFile)) {
+        url <- paste0("http://www.shfe.com.cn/data/dailydata/kx/pm", tradingDay, ".dat")
+
+        if (class(try(r <- GET(url))) == 'try-error') next
+
+        page <- content(r, 'text')
+
+        jsonFile <- fromJSON(page)
+        ## summary(jsonFile)
+        #             Length Class  Mode     
+        # o_cursor    759    -none- list        :查询得到的数据
+        # o_code        1    -none- numeric     :查询结果状态
+        # o_msg         1    -none- character   :查询信息: "成交、持仓排名查询成功"
+        # report_date   1    -none- character   :交易日期
+        # update_date   1    -none- character   :更新时间
+        # print_date    1    -none- character   :打印时间
+        rankData <- jsonFile$o_cursor
+
+        dt <- lapply(1:length(rankData), function(i){
+            as.data.table(rankData[[i]])
+            }) %>% rbindlist()
+        dt[, INSTRUMENTID := gsub(' ', '', INSTRUMENTID)]
+        dt[nchar(PARTICIPANTABBR1) == 0 & nchar(PARTICIPANTABBR2) == 0 & nchar(PARTICIPANTABBR3) == 0, 
+           ":="(PARTICIPANTABBR1 = "合计",
+                PARTICIPANTABBR2 = "合计",
+                PARTICIPANTABBR3 = "合计")]
+        dt[, ":="(PARTICIPANTID1 = NULL, 
+                  PARTICIPANTID2 = NULL, 
+                  PARTICIPANTID3 = NULL,
+                  PRODUCTSORTNO = NULL,
+                  PRODUCTNAME = NULL)]
+        names(dt) <- c('合约代码','名次',
+                       '期货公司会员简称','成交量','比上交易日增减',
+                       '期货公司会员简称','持买单量','比上交易日增减',
+                       '期货公司会员简称','持卖单量','比上交易日增减')
+        print(dt)
+        if (nrow(dt) != 0) fwrite(dt, destFile)
+    }
+    ## =========================================================================
 }
-##------------------------------------------------------------------------------
-
-
-################################################################################
-## SHFE: 上期所
-exchURL <- "http://www.shfe.com.cn/statements/dataview.html?paramid=pm&paramdate="
-################################################################################
-
-
-################################################################################
-## 后台开启一下命令
-## 
-## cd Desktop
-## java -jar selenium-server-standalone-3.0.0.jar
-## 
-################################################################################
-remDr <- remoteDriver(remoteServerAddr ='localhost'
-                      ,port = 4444
-                      ,browserName = 'firefox')
-remDr$getStatus()
-# 
-# 
-################################################################################
-## 开始下载数据
-## 1.持仓排名
-################################################################################
-
-shfeData <- function(i) {
-  ## ===========================================================================
-  tempDir <- paste0(dataPath,exchCalendar[i,calendarYear])
-
-  if (!dir.exists(tempDir)) dir.create(tempDir, recursive = TRUE)
-  ## ===========================================================================
-  tempURL <- paste0(exchURL, exchCalendar[i,days])
-
-  ## ===========================================================================
-  ## 判断文件是不是已经下载了
-  ## ---------------------------------------------------------------------------
-  destFile <- paste0(tempDir, "/",
-                     ChinaFuturesCalendar[i,days],".xlsx")
-
-  if (file.exists(destFile)) return(NULL)
-  ## ===========================================================================
-  
-  ## ===========================================================================
-  ## 开始准备下载数据
-  # 需要保持开启
-  # ----------------------------------------------------------------------------
-  remDr$open(silent = TRUE)
-  remDr$navigate(tempURL)
-  Sys.sleep(1)
-
-  ## ---------------------------------------------------------------------------
-  tempTitle <- remDr$findElements(using = 'id', value = 'datatitle')[[1]]
-  tempQueryDay <- tempTitle$getElementAttribute('outerHTML')[[1]] %>% 
-    read_html(encoding = 'GB18030') %>% 
-    html_nodes('table') %>% 
-    html_table() %>% 
-    .[[1]] %>% 
-    .[2, 'X1'] %>% 
-    gsub('-','',.)
-  if (tempQueryDay != exchCalendar[i,days]) return(NULL)
-  ## ---------------------------------------------------------------------------
-
-  temp <- remDr$findElements(using = 'id', value = 'li_all')[[1]]
-  #-- 点击选择全部合约
-  tempWeb <- temp$clickElement()
-  Sys.sleep(1)
-  #-- 找到数据
-  tempData <- remDr$findElements(using = 'id', value = 'addedtable')[[1]]
-  
-  webData <- tempData$getElementAttribute('outerHTML')[[1]] %>% 
-    read_html() %>% 
-    html_node('table') %>% 
-    html_table(fill = TRUE)
-
-  tryNo <- 0
-  while ( (!file.exists(destFile) | file.size(destFile) < 1000) & (tryNo < 10) ){
-    openxlsx::write.xlsx(webData, file = destFile,
-                         colNames = FALSE, rowNames = FALSE)
-    tryNo <- tryNo + 1
-  }
-  
-  ## ===========================================================================
-  ## 关闭浏览器
-  try({
-    system('pkill -f firefox')
-    system('pkill -f geckodriver')
-    system('rm -rf /tmp/rust_mozprofile*')
-  })
-  ## ===========================================================================
-}
-
-## =============================================================================
-sapply(1:nrow(ChinaFuturesCalendar),shfeData)
-## =============================================================================
