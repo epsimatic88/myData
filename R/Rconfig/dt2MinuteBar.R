ed8ae2983bae27a734037b35564ca1a12f4c0f32
williamlfang <william.lian.fang@gmail.com>
2018-01-12 19:39:02 +0800

    完成从 tick 数据汇总到 分钟 数据的函数

--- R/Rconfig/dt2MinuteBar.R
+++ R/Rconfig/dt2MinuteBar.R
@@ -3,48 +3,45 @@
 ## 功能：
 ## 用于把 tick data 的数据转化为 分钟 的数据，
 ## 1. dt2MinuteBar(dt)
-##############################################################################
-##----------------------------------------------------------------------------
+################################################################################
+##------------------------------------------------------------------------------
 dt2MinuteBar <- function(dt){
   setkey(dt,InstrumentID)
   temp <- lapply(unique(dt$InstrumentID), function(ii){ dt[ii] })
 
-  no.cores <- max(round(detectCores()/3), 4)
-  # no.cores <- max(round(detectCores()/4), 4)
+  no.cores <- max(round(detectCores()/4), 8)
   cl <- makeCluster(no.cores, type="FORK")
-  # clusterExport(cl, c("dt","temp"))
-  # clusterEvalQ(cl,{library(data.table);library(magrittr)})
   dtMinute <- parLapply(cl, 1:length(temp), function(ii){
     ## -------------------------------------------------------------------------
-    temp[[ii]] %>%
+    tempRes <- temp[[ii]] %>%
       .[, .SD[,.(
-        #-----------------------------------------------------------------------------
+        #-----------------------------------------------------------------------
         NumericExchTime = .SD[1,NumericExchTime],
-        #-----------------------------------------------------------------------------
-        OpenPrice = .SD[DeltaVolume != 0][1,LastPrice],
-        HighPrice = .SD[DeltaVolume != 0, max(LastPrice, na.rm=TRUE)],
-        LowPrice  = .SD[DeltaVolume != 0, min(LastPrice, na.rm=TRUE)],
+        #-----------------------------------------------------------------------
+        OpenPrice  = .SD[DeltaVolume != 0][1,LastPrice],
+        HighPrice  = .SD[DeltaVolume != 0, max(LastPrice, na.rm=TRUE)],
+        LowPrice   = .SD[DeltaVolume != 0, min(LastPrice, na.rm=TRUE)],
         ClosePrice = ifelse(nrow(.SD[DeltaVolume != 0]) != 0,
                       .SD[DeltaVolume != 0][nrow(.SD[DeltaVolume != 0]), LastPrice],
                       .SD[.N,LastPrice]),
-        #-----------------------------------------------------------------------------
+        #-----------------------------------------------------------------------
         Volume            = sum(.SD$DeltaVolume, na.rm=TRUE),
         Turnover          = sum(.SD$DeltaTurnover, na.rm=TRUE),
-        #                 -----------------------------------------------------------------------------
+        #                 ------------------------------------------------------
         OpenOpenInterest  = .SD[1,OpenInterest],
-        HighOpenInterest  =.SD[,max(OpenInterest, na.rm=TRUE)],
+        HighOpenInterest  = .SD[,max(OpenInterest, na.rm=TRUE)],
         LowOpenInterest   = .SD[,min(OpenInterest, na.rm=TRUE)],
         CloseOpenInterest = .SD[.N,OpenInterest],
-        #                 -----------------------------------------------------------------------------
+        #                 ------------------------------------------------------
         UpperLimitPrice   = unique(na.omit(.SD$UpperLimitPrice)),
         LowerLimitPrice   = unique(na.omit(.SD$LowerLimitPrice)),
         SettlementPrice   = .SD[.N, SettlementPrice]
       )], by = .(TradingDay, InstrumentID, Minute)] %>%
-      .[Volume != 0 & Turnover != 0]
+      .[Volume != 0]
+    return(tempRes)
     ## -------------------------------------------------------------------------
   }) %>% rbindlist()
   stopCluster(cl)
-
   return(dtMinute)
 }
-##############################################################################
+################################################################################
