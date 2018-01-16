40243495ff86404f1729423841d80ac78fd28e85
williamlfang <william.lian.fang@gmail.com>
2018-01-16 10:17:12 +0800

    增加市场指数的数据库

---
+++ mysql/MarketIndex.sql
@@ -0,0 +1,45 @@
+################################################################################
+## MySQL:
+## 用于建立 MySQL 数据库命令
+## 
+## 包括:
+## MarketIndex
+## 
+## 注意:
+##
+## Author: fl@hicloud-investment.com
+## CreateDate: 2018-01-03
+################################################################################
+
+CREATE DATABASE `MarketIndex` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+## =============================================================================
+## 南华商品指数
+CREATE TABLE MarketIndex.stocks_index(
+    TradingDay      DATE            NOT NULL,
+    stockID         CHAR(30)        NOT NULL,           ## 股票代码
+    stockName       CHAR(30)        NOT NULL,           ## 股票名称
+    ## -------------------------------------------------------------------------
+    open            DECIMAL(10,3),
+    high            DECIMAL(10,3),
+    low             DECIMAL(10,3),
+    close           DECIMAL(10,3),
+    volume          BIGINT,
+    turnover        DECIMAL(30,3),
+    ## -------------------------------------------------------------------------
+    PRIMARY KEY (TradingDay, stockID)
+) DEFAULT CHARSET=utf8;
+## =============================================================================
+
+
+
+## =============================================================================
+## 南华商品指数
+CREATE TABLE MarketIndex.Nanhua (
+    TradingDay     DATE           NOT NULL,
+    close          DECIMAL(10,3)  NULL,
+    PRIMARY KEY (TradingDay)
+) DEFAULT CHARSET=utf8;
+## =============================================================================
+
