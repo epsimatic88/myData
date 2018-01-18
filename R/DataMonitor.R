a609a11ec699b4a2de26e9afb00b4d217643b987
williamlfang <william.lian.fang@gmail.com>
2018-01-18 09:56:38 +0800

    修改数据监控脚本,实现针对每个交易所的持仓数据进行入库监控

--- R/DataMonitor.R
+++ R/DataMonitor.R
@@ -20,9 +20,9 @@ ChinaFuturesCalendar <- dbGetQuery(mysql, "
             ) %>% as.data.table()
 
 if (as.numeric(format(Sys.time(),'%H')) < 17){
-  currTradingDay <- ChinaFuturesCalendar[days == format(Sys.Date(),'%Y-%m-%d')]
+  currTradingDay <- ChinaFuturesCalendar[days <= format(Sys.Date(),'%Y-%m-%d')][.N]
 }else{
-  currTradingDay <- ChinaFuturesCalendar[nights == format(Sys.Date(),'%Y-%m-%d')]
+  currTradingDay <- ChinaFuturesCalendar[days > format(Sys.Date(),'%Y-%m-%d')][1]
 }
 lastTradingDay <- ChinaFuturesCalendar[days < currTradingDay[1,days]][.N]
 
@@ -90,7 +90,7 @@ dtMinute <- dbGetQuery(mysql,paste("
 mysql <- mysqlFetch('vnpy')
 dtBreakTime <- dbGetQuery(mysql, "
             SELECT *
-            FROM breakTime_YunYang1_FromPC"
+            FROM breakTime_TianMi1_FromAli"
             ) %>% as.data.table() %>%
             .[TradingDay == currTradingDay[1,days]]
 if (nrow(dtBreakTime) != 0) {
@@ -138,31 +138,82 @@ if (nrow(dtBreakTime) != 0) {
 ## =============================================================================
 ## china_futures_bar
 ## =============================================================================
-mysql <- mysqlFetch('china_futures_bar')
-dtOiRank <- dbGetQuery(mysql,paste("
-            SELECT TradingDay, count(*) as recordingNo
-            FROM oiRank
-            WHERE TradingDay >= ", format(Sys.Date()-30,"%Y%m%d"),
-            "GROUP BY TradingDay")
-            ) %>% as.data.table()
+# mysql <- mysqlFetch('china_futures_bar')
+# dtOiRank <- dbGetQuery(mysql,paste("
+#             SELECT TradingDay, count(*) as recordingNo
+#             FROM oiRank
+#             WHERE TradingDay >= ", format(Sys.Date()-30,"%Y%m%d"),
+#             "GROUP BY TradingDay")
+#             ) %>% as.data.table()
+# cat("## ====================================== ##\n")
+# cat("## china_futures_bar.oiRank \n")
+# cat('## \n')
+
+# mysql <- mysqlFetch('china_futures_bar')
+# dceProducts <- data.table(id = seq(1:16),
+#                      conName = c('a','b','m','y','p','c','cs','jd',
+#                                  'fb','bb','l','v','pp','j','jm','i')
+#                      )
+# allInstrumentIDNum <- dbGetQuery(mysql,
+# paste("select distinct InstrumentID
+#        from minute
+#        where tradingday = ", gsub('-','',lastTradingDay[1,days]),
+#       "and (volume != 0 or closeopeninterest != 0)")) %>% as.data.table() %>%
+#   .[,":="(ContractID = gsub("[0-9]","",InstrumentID))] %>%
+#   merge(.,dceProducts, by.x = 'ContractID', by.y = 'conName')
+# tempCurrFiles <- list.files(paste0("~/myCodes/ExchDataFetch/data/positionRank/DCE/",
+#                                   substr(lastTradingDay[1,days], 1,4),"/"), pattern =
+#                              paste0(lastTradingDay[1,gsub('-','',days)],".*")) %>%
+#   gsub(as.character(lastTradingDay[1,gsub('-','',days)]), '', .) %>%
+#   gsub('_|\\.xlsx','',.)
+# notInFiles <- rep(NA,nrow(allInstrumentIDNum))
+# for (i in 1:nrow(allInstrumentIDNum)) {
+#   if (!(allInstrumentIDNum[i, InstrumentID] %in% tempCurrFiles))
+#     notInFiles[i] <- allInstrumentIDNum[i, InstrumentID]
+# }
+# ## -----------------------------------------------------------------------------
+# ## 1. 如果当前交易日不在数据
+# ## 2. 或者当前数据缺失
+# if (! lastTradingDay[1,days] %in% dtOiRank[,TradingDay] |
+#   dtOiRank[.N, recordingNo < .95 * mean(recordingNo)] |
+#   len(notInFiles[!is.na(notInFiles)]) != 0) {
+#   cat('## 当前交易日的数据未入库！！！\n')
+#   cat('## 请检查程序。\n')
+#   cat('## DCE 未入库数据含：')
+#   print(data.table(InstrumentID = notInFiles[!is.na(notInFiles)]))
+#   cat('## 程序脚本位于：==> /home/fl/myCodes/ExchDataFetch/ \n')
+#   cat("## ====================================== ##\n\n")
+# }else{
+#   cat('## 当前交易日的数据已入库！！！\n')
+#   print(dtOiRank)
+#   cat('\n')
+#   cat("## ====================================== ##\n\n")
+# }
+
+
 cat("## ====================================== ##\n")
 cat("## china_futures_bar.oiRank \n")
 cat('## \n')
-## -----------------------------------------------------------------------------
-## 1. 如果当前交易日不在数据
-## 2. 或者当前数据缺失
-if (! lastTradingDay[1,days] %in% dtOiRank[,TradingDay] |
-  dtOiRank[.N, recordingNo < .95 * mean(recordingNo)]) {
-  cat('## 当前交易日的数据未入库！！！\n')
-  cat('## 请检查程序。\n')
-  cat('## 程序脚本位于：==> /home/fl/myCodes/ExchDataFetch/ \n')
-  cat("## ====================================== ##\n\n")
-}else{
-  cat('\n## 当前交易日的数据已入库！！！\n')
-  print(dtOiRank)
-  cat('\n\n')
-  cat("## ====================================== ##\n\n")
+
+tempTradingDay <- currTradingDay[1, gsub('-', '', days)]
+check_oi <- function(exchID) {
+  tempDir <- paste0("/home/fl/myData/data/oiRank/data/", toupper(exchID), "/2018")
+  tempPattern <- ifelse(exchID == 'czce', '\\.xls', '\\.csv')
+  tempFiles <- list.files(tempDir, pattern = tempPattern) %>% grep(tempTradingDay, .)
+
+  cat("\n## ====================================== ##\n")
+  if (length(tempFiles) == 0) {
+    cat(paste(exchID, '数据未入库'))
+  } else {
+    cat(paste(exchID, '数据已入库'))
+  }
+  cat("\n## ====================================== ##\n")
 }
+
+for (id in c('cffex','czce','dce','shfe')) {
+  check_oi(id)
+}
+
 
 
 ## =============================================================================
@@ -217,7 +268,8 @@ if (! lastTradingDay[1,days] %in% dtVolumeMultiple[,TradingDay] |
 ## HiCloud
 ## =============================================================================
 
-accountInfo <- data.table(accountID = c('TianMi1','TianMi3','YunYang1'))
+accountInfo <- data.table(accountID = c('TianMi1','TianMi2','TianMi3','YunYang1'))
+
 checkSignal <- function(accountID) {
   mysql <- mysqlFetch(accountID)
   tradingSignal <- dbGetQuery(mysql, "select * from tradingSignal order by InstrumentID") %>%
